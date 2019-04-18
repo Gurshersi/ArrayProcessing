ad0a334a09018f9636d009935cde9c4885aac220
Gursher Singh <715837@student.tor.loc>
2019-04-18 14:01:55 -0400

    first commit of the day

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = new int[100];
+            int[] frequency = new int[100];
+            int n, i, j, ctr;
+
+
+            Console.Write("Give the input of number of elements to be stored in the array :");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input {0} elements in the array :\n", n);
+        }
+    }
+}
