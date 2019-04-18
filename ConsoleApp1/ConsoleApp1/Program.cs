c993c502d978f58e67c4a5be1a72755d02976888
Gursher Singh <715837@student.tor.loc>
2019-04-18 14:40:48 -0400

    commit at 2:40 PM

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -19,6 +19,39 @@ namespace ConsoleApp1
             n = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Input {0} elements in the array :\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                array1[i] = Convert.ToInt32(Console.ReadLine());
+                frequency[i] = -1;
+            }
+            for (i = 0; i < n; i++)
+            {
+                ctr = 1;
+                for (j = i + 1; j < n; j++)
+                {
+                    if (array1[i] == array1[j])
+                    {
+                        ctr++;
+                        frequency[j] = 0;
+                    }
+                }
+
+                if (frequency[i] != 0)
+                {
+                    frequency[i] = ctr;
+                }
+            }
+            Console.Write("\nThe frequency of all elements of the array : \n");
+            for (i = 0; i < n; i++)
+            {
+                if (frequency[i] != 0)
+                {
+                    Console.Write("{0} occurs {1} times\n", array1[i], frequency[i]);
+                }
+            }
+
+
         }
     }
 }
